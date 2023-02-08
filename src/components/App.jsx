ea2fdb3a3a06e2223ce2a3a17ac83d233e859138
Shahvez Ahmad <shahvez.ahmad2002@gmail.com>
2023-02-08 13:21:06 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { CssBaseline } from '@mui/material';
+import { Route,Switch } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+    <CssBaseline/>
+    <h1> Hello World!</h1>
+    </div>
+  )
+  
+}
+
+export default App
