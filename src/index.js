875149dd8d118f9c7bf628f63900a312b8029666
Shahvez Ahmad <shahvez.ahmad2002@gmail.com>
2023-02-28 16:50:59 +0530

    scrollbar fix

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './app/store';
+import './index.css';
+
 const theme = createTheme({});
 const root = document.getElementById('root');
 
