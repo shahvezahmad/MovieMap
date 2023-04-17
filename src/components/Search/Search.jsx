9d93b4c786e683689c6407d7accac85b4a783720
Shahvez Ahmad <shahvez.ahmad2002@gmail.com>
2023-04-18 01:07:48 +0530

    error fix

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,16 +1,18 @@
-import React, {useState, useEffect } from 'react';
+import React, {useState } from 'react';
 import { TextField, InputAdornment } from '@mui/material';
 import { Search as SearchIcon } from '@mui/icons-material';
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import useStyles from './styles';
 import { searchMovie } from '../../features/currentGenreOrCategory';
+
 const Search = () => {
     const classes = useStyles();
     const [query,setQuery] = useState('');
     const dispatch = useDispatch();
     const location = useLocation();
 
+    //press enter to search a movie
     const handleKeyPress =(event) => {
         if(event.key === 'Enter'){
             dispatch(searchMovie(query));
