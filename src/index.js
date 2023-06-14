02e04ccdd72e313a01509801327186607b0c297d
Shahvez Ahmad <shahvez.ahmad2002@gmail.com>
2023-06-14 22:52:53 +0530

    added comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+//imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './app/store';
-
 import ToggleColorModeProvider from './utils/ToggleColorMode';
 import './index.css';
 
@@ -12,9 +12,9 @@ const root = document.getElementById('root');
 
 ReactDOM.createRoot(root).render(
     <Provider store = {store}>
-        <ToggleColorModeProvider>
-            <BrowserRouter>
-                <App />
+        <ToggleColorModeProvider> //light and dark mode
+            <BrowserRouter> //enable routing
+                <App /> //render the app component
             </BrowserRouter>,
         </ToggleColorModeProvider>
     </Provider>,
